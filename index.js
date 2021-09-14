7c7dc1f65db5d6a1b4306687fc31e088f69687ec
yogesh16101999 <chavanyogesh981@gmail.com>
2021-09-15 01:19:28 +0530

    heroku2

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const postRoutes = require('./routes/posts.js');
-const port = process.env.port || 5000;
+const dotenv=require('dotenv');
+
+
+
 const app = express();
+dotenv.config();
 const url = 'mongodb://yogesh:021479@cluster0-shard-00-00.v9ss7.mongodb.net:27017,cluster0-shard-00-01.v9ss7.mongodb.net:27017,cluster0-shard-00-02.v9ss7.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-lmn4zd-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 app.use(cors());
@@ -19,7 +23,7 @@ app.get('/',(req,res)=>{
 
 
 app.use('/posts', postRoutes);
-
+const port = process.env.port || 5000;
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('sucessful'))
     .catch((err) => console.log(err));
