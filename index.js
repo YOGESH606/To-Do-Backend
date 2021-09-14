855a31c829612a13339ba08c2f1572e68b16b923
yogesh16101999 <chavanyogesh981@gmail.com>
2021-09-15 01:39:12 +0530

    heroku5

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ const dotenv = require('dotenv');
 
 const app = express();
 dotenv.config();
-const url = 'mongodb://yogesh:021479@cluster0-shard-00-00.v9ss7.mongodb.net:27017,cluster0-shard-00-01.v9ss7.mongodb.net:27017,cluster0-shard-00-02.v9ss7.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-lmn4zd-shard-0&authSource=admin&retryWrites=true&w=majority';
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
-const port = process.env.port || 5000;
-app.listen(port, () => console.log(`server is running on port:${port} and db is connetced`))
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('req received suceesfylly')
 })
 
-
 app.use('/posts', postRoutes);
+const PORT = process.env.PORT || 7000;
 
+
+
+const url = 'mongodb://yogesh:021479@cluster0-shard-00-00.v9ss7.mongodb.net:27017,cluster0-shard-00-01.v9ss7.mongodb.net:27017,cluster0-shard-00-02.v9ss7.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-lmn4zd-shard-0&authSource=admin&retryWrites=true&w=majority';
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('sucessful'))
+    .then(() => app.listen(PORT, () => console.log(`server is running on port:${PORT} and db is connetced`)) )
     .catch((err) => console.log(err));
